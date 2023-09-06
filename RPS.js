6ada7d106588e908e811551521861f41c6a3da78
Shivam Gupta <112889717+GoldReigner@users.noreply.github.com>
2023-09-06 12:33:00 +0530

    Play with Keyboard

--- RPS.js
+++ RPS.js
@@ -18,7 +18,26 @@ updateScoreElements();
 
 
 
+document.querySelector('.js-rock-button').addEventListener('click',() => {
+    playGame('rock');
+});
+document.querySelector('.js-paper-button').addEventListener('click',() => {
+    playGame('paper');
+});
+document.querySelector('.js-scissors-button').addEventListener('click',() => {
+    playGame('scissors');
+});
 
+document.body.addEventListener('keydown',(event)=>{
+if(event.key==='r'){
+    playGame('rock');
+}else if(event.key==='p'){
+    playGame('paper');
+}else if(event.key==='s'){
+    playGame('scissors');
+}
+
+});
 
 function playGame(playerMove){
 const computermove= pickComputerMove();
